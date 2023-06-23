6cf2f21c13d07f16c06301224f51b1f15fe42dce
Jhenifer Dias <jheniferdias1@outlook.com>
2023-06-23 17:43:27 -0300

    🚧 - Updated VC and Cell

---
+++ GloboPlayChallange/Scenes/Home/View/Cell/FavoritesTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  FavoritesTableViewCell.swift
+//  GloboPlayChallange
+//
+//  Created by Jhenifer Dias on 22/06/23.
+//
+
+import UIKit
+
+class FavoritesCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .red
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
